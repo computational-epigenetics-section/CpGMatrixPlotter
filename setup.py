bef3a6a8d04728474cc4c63ce52004d8862f8b9a
Anthony Scott <ascott329@gmail.com>
2018-05-09 11:58:44 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="cpgPlotter",
+      version="0.1.0",
+      description="Package to plot cpg data",
+      author="Anthony Scott, PhD",
+      author_email="charles.scott@bcm.edu",
+      license='MIT',
+      packages=['cpgPlotter'],
+      install_requires=['numpy', 'matplotlib'],
+      )
