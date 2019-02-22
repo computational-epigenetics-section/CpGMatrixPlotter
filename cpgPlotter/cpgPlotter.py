8f2ed4fb3e86c42c5d9c0a78f064600d14885c9a
Anthony Scott <ascott329@gmail.com>
2019-02-22 10:28:04 -0600

    changed layout to better present figure

--- cpgPlotter/cpgPlotter.py
+++ cpgPlotter/cpgPlotter.py
@@ -4,12 +4,25 @@ import pandas as pd
 
 
 class CpGMatrixPlotter:
+    """
+    Main class used to plot CpG matrix data
+
+    Example:
+    >>>from cpgPlotter import CpgMatrixPlotter
+    >>>import numpy as np
+    >>>data = np.array([[1,1,1,0],
+    >>>                 [1,1,0,0]])
+    >>>locations = np.array([100, 110, 155, 190])
+
+    >>>plotter = CpgMatrixPlotter()
+    >>>plotter.plotCpgMatrix(data, locations)
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
-    def buffer_spacings(spacings, radius):
+    def _buffer_spacings(spacings, radius):
         for i in range(len(spacings)):
             if i == 0:
                 continue
@@ -20,7 +33,7 @@ class CpGMatrixPlotter:
         return np.array(spacings)
 
     @staticmethod
-    def get_range_from_bin(bin_str: str, bin_size=100):
+    def _get_range_from_bin(bin_str: str, bin_size=100):
         chrom, loc = bin_str.split("_")
         loc = int(loc)
         start = loc - bin_size
@@ -42,7 +55,7 @@ class CpGMatrixPlotter:
         return cpgMatrix, cpgPositions, cluster_labels, input_labels
 
     @staticmethod
-    def get_color(cpg_value: int):
+    def _get_color(cpg_value: int):
         if cpg_value == 1:
             return "black"
         elif cpg_value == 0:
@@ -52,41 +65,50 @@ class CpGMatrixPlotter:
             NotImplementedError("I cannot yet accept unknown values. But I will soon.")
 
     @staticmethod
-    def sort_matrix_by_methylation(cpgMatrix):
+    def _sort_matrix_by_methylation(cpgMatrix):
         df = pd.DataFrame(cpgMatrix)
         df['mean'] = df.apply(np.mean, axis=1)
         df = df.sort_values(['mean'], ascending=True)
 
         return np.array(df.drop(['mean'], axis=1))
 
-
     def plotCpGMatrix(self, cpgMatrix, cpgPositions, title=None, figsize=(8, 8), sort=False):
+        """
+        Main plotting function. Plot tanghulu plot of cpg matrix data
+
+        :param cpgMatrix: np.array of CpG values. 1=methylated; 0=unmethylated
+        :param cpgPositions: genomic positions of the CpGs. Used to calculate spacing
+        :param title: Title to add to the plot, optional
+        :param figsize: size of the plot to generate, default (8,8)
+        :param sort: sort the reads my methylation value
+        :return: matplotlib axes
+        """
         fig, ax = plt.subplots(figsize=figsize)
         v_steps = 1 / cpgMatrix.shape[0]
         v_spacings = np.arange(0, 1, v_steps)
         h_spacings = (cpgPositions - min(cpgPositions)) * 0.01
-        ax.set_ylim(-.1, 1.1)
-        ax.set_xlim(-.1, 1.1)
+        ax.set_ylim(-.02, 1.02)
+        ax.set_xlim(-.02, max(h_spacings) + 0.02)
         ax.set_xticks(h_spacings)
         ax.set_xticklabels(cpgPositions, rotation=90)
         ax.set_yticks([])
         if title:
             ax.set_title(title)
         radius = min(v_steps / 2.5, 0.05)
-        h_spacings = self.buffer_spacings(h_spacings, radius * 2)
+        h_spacings = self._buffer_spacings(h_spacings, radius * 2)
 
         if sort:
-            cpgMatrix = self.sort_matrix_by_methylation(cpgMatrix)
+            cpgMatrix = self._sort_matrix_by_methylation(cpgMatrix)
 
         for read, vspace in zip(cpgMatrix, v_spacings):
             ax.axhline(vspace, color="black", zorder=1)
             for cpg, hspace in zip(read, h_spacings):
                 x = hspace
                 y = vspace
-                circle = plt.Circle((x, y), radius=radius, facecolor=self.get_color(cpg), edgecolor="black")
+                circle = plt.Circle((x, y), radius=radius, facecolor=self._get_color(cpg), edgecolor="black")
                 ax.add_artist(circle)
 
-        return fig
+        return ax
 
     def plotCpGReadDataFrame(self, clustered_data_frame, title=None, figsize=(8, 8), sort=True):
         """
